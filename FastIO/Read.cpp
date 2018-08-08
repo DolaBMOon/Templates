fdaa12fe55eab49d432a96bdc6f93b8ad8393919
DOlaBMOon <dolabmoon@gmail.com>
2018-08-08 14:21:37 +0800

    fread

---
+++ FastIO/Read.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+const int _B=100000;
+
+char Nc()
+{
+	static char buf[_B],*p=buf,*q=buf;
+	return ((p==q)&&(q=buf+fread(buf,1,_B,stdin))==(p=buf))?EOF:*(p++);
+}
+
+int Read()
+{
+	char ch;
+	while(!isdigit(ch=Nc()));
+	int sum=ch-48;
+	while(isdigit(ch=Nc()))
+		sum=sum*10+ch-48;
+	return sum;
+}
+
+int main()
+{
+	return 0;
+}
