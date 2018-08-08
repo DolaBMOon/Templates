eb98f09101bbc0fafb0ec190ef259bc0ca250783
DOlaBMOon <dolabmoon@gmail.com>
2018-08-08 20:59:04 +0800

    Simpson, eps

--- Simpson/Simpson.cpp
+++ Simpson/Simpson.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+const double eps=1e-6;
+
 double f(double x)
 {
 	//It's what to be Integrated.
@@ -13,7 +15,7 @@ double Simpson(double l,double m,double r,double fl,double fm,double fr)
 	return (r-l)*(fl+4*fm+fr)/6;
 }
 
-double Integrate(double l,double m,double r,double fl,double fm,double fr,double nw,double eps,int deep)
+double Integrate(double l,double m,double r,double fl,double fm,double fr,double nw,int deep)
 {
 	if(deep>=20)
 		return nw;
@@ -22,16 +24,16 @@ double Integrate(double l,double m,double r,double fl,double fm,double fr,double
 	double x=Simpson(l,lm,m,fl,flm,fm),y=Simpson(m,rm,r,fm,frm,fr);
 	if(abs(x+y-nw)<eps)
 		return nw;
-	return Integrate(l,lm,m,fl,flm,fm,x,eps,deep+1)+
-	Integrate(m,rm,r,fm,frm,fr,y,eps,deep+1);
+	return Integrate(l,lm,m,fl,flm,fm,x,deep+1)+
+	Integrate(m,rm,r,fm,frm,fr,y,deep+1);
 }
 
-double Calc(double l,double r,double eps)
+double Calc(double l,double r)
 {
 	double m=(l+r)*0.5;
 	double fl=f(l),fm=f(m),fr=f(r);
 	double nw=Simpson(l,m,r,fl,fm,fr);
-	return Integrate(l,m,r,fl,fm,fr,nw,eps,0);
+	return Integrate(l,m,r,fl,fm,fr,nw,0);
 }
 
 int main()
