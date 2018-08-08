9fdc174f63f91cdacf0a548bfb16cca37b90ac7a
DOlaBMOon <dolabmoon@gmail.com>
2018-08-08 14:51:57 +0800

    MulMod by asm ;-)

---
+++ Tricks/MulMod.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int MOD=998244353;
+
+inline int Mul(int a,int b)
+{
+	unsigned long long x=(long long)a*b;
+	unsigned xh=(unsigned)(x>>32),xl=(unsigned)x,d,m;
+	asm
+		(
+		 "divl %4;\n\t"
+		 :"=a"(d),"=d"(m)
+		 :"d"(xh),"a"(xl),"r"(MOD)
+		);
+	return m;
+}
+
+int main()
+{
+	return 0;
+}
