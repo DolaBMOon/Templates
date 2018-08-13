55df95e6158eefd8ad7a53233f04aa242bcf024e
DOlaBMOon <dolabmoon@gmail.com>
2018-08-13 12:33:10 +0800

    optimize

--- Header/Header.cpp
+++ Header/Header.cpp
@@ -1,3 +1,13 @@
+//%:pragma GCC optimize("Ofast")
+//%:pragma GCC target("sse3","sse2","sse")
+//%:pragma GCC target("avx","sse4","sse4.1","sse4.2","ssse3")
+//%:pragma GCC target("f16c")
+//%:pragma GCC optimize("inline","fast-math","unroll-loops","no-stack-protector")
+//%:pragma GCC diagnostic error "-fwhole-program"
+//%:pragma GCC diagnostic error "-fcse-skip-blocks"
+//%:pragma GCC diagnostic error "-funsafe-loop-optimizations"
+//%:pragma GCC diagnostic error "-std=c++14"
+#include<Oops.h>
 #include<iostream>
 #include<algorithm>
 #include<cstdio>
