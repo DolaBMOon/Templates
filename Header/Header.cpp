a5f9c9e5c4c1f8865b8959dc7b41c7df874b9bce
DOlaBMOon <dolabmoon@gmail.com>
2018-08-13 12:29:07 +0800

    Header

---
+++ Header/Header.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+
+using namespace std;
+
+#define Whats(x) cout<<#x<<" is "<<x<<endl
+#define Divhe() cout<<">>>>>>>>>>>>>>>"<<endl
+#define Divshe() cout<<"<<<<<<<<<<<<<<<"<<endl
+
+/*
+That's how everything starts.
+*/
+
+int main()
+{
+	return 0;
+}
