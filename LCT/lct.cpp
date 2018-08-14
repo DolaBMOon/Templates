f12898c023cae8be569f4a3462a829146bb55e67
DOlaBMOon <dolabmoon@gmail.com>
2018-08-14 15:57:38 +0800

    about DS..

---
+++ LCT/lct.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<cstdio>
+
+using namespace std;
+
+const int N=500000+10;
+const int INF=0x3f3f3f3f;
+
+int n,m;
+
+int ch[N][2],fa[N],lz[N],u[N],v[N],sm[N],w[N];
+
+int Which(int x)
+{
+	return ch[x][1]==x;
+}
+
+void Apply(int x)
+{
+	lz[x]^=1;
+	swap(ch[x][0],ch[x][1]);
+}
+
+bool isRt(int x)
+{
+	return ch[fa[x]][0]!=x&&ch[fa[x]][1]!=x;
+}
+
+void Update(int x)
+{
+	sm[x]=w[x]+sm[ch[x][0]]+sm[ch[x][1]];
+}
+
+void Rotate(int x)
+{
+	register int f=fa[x],ff=fa[f],k=Which(x);
+	if(!isRt(f))
+	{
+		ch[ff][Which(f)]=x;
+		Update(ff);
+	}
+	fa[x]=ff;
+	ch[f][k]=ch[x][!k];
+	fa[ch[f][k]]=f;
+	Update(f);
+	ch[x][!k]=f;
+	fa[f]=x;
+	Update(x);
+}
+
+void Pushdown(int x)
+{
+	if(x&&lz[x])
+	{
+		if(ch[x][0])
+			Apply(ch[x][0]);
+		if(ch[x][1])
+			Apply(ch[x][1]);
+		lz[x]=0;
+	}
+}
+
+void Climb(int x)
+{
+	if(!isRt(x))
+		Climb(fa[x]);
+	Pushdown(x);
+}
+
+void Splay(int x)
+{
+	Climb(x);
+	for(int f;!isRt(x);Rotate(x))
+		if(!isRt(f=fa[x]))
+			Rotate((Which(f)==Which(x))?f:x);
+}
+
+void Access(int o)
+{
+	for(int y=0,x=o;x;y=x,x=fa[x])
+	{
+		Splay(x);
+		ch[x][1]=y;
+		Update(x);
+	}
+	Splay(o);
+}
+
+void MkRt(int x)
+{
+	Access(x);
+	Apply(x);
+}
+
+void Link(int x,int y)
+{
+	MkRt(x);
+	fa[x]=y;
+}
+
+void Cut(int x,int y)
+{
+	MkRt(x);
+	Access(y);
+	ch[y][0]=fa[x]=0;
+	Update(y);
+}
+
+int main()
+{
+	return 0;
+}
