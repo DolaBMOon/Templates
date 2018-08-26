df0559e62b7a9552ab26c9869e4af60aa7baa02b
DOlaBMOon <dolabmoon@gmail.com>
2018-08-26 14:13:43 +0800

    Mod

---
+++ Tricks/Mod.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int MOD=998244353;
+
+int Mul(int a,int b)
+{
+	unsigned long long x=(long long)a*b;
+	unsigned xh=(unsigned)(x>>32),xl=(unsigned)x,d,m;
+	asm
+		(
+		 "divl %4;\n\t"
+		 :"=a"(d),"=d"(m)
+		 :"d"(xh),"a"(xl),"r"(MOD)
+		);
+	return m;
+}
+
+int U(int x,int y)
+{
+	return ((x+=y)>=MOD)?(x-MOD):x;
+}
+
+void SU(int& x,int y)
+{
+	((x+=y)>=MOD)?(x-=MOD):0;
+}
+
+int D(int x,int y)
+{
+	return ((x-=y)<0)?(x+MOD):x;
+}
+
+void SD(int& x,int y)
+{
+	((x-=y)<0)?(x+=MOD):0;
+}
+
+int main()
+{
+	return 0;
+}
