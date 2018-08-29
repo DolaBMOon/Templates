e41b48c41ed908281aec399d9f78bfc73f2150b2
DOlaBMOon <dolabmoon@gmail.com>
2018-08-29 15:30:15 +0800

    Read.cpp

--- FastIO/Read.cpp
+++ FastIO/Read.cpp
@@ -2,28 +2,24 @@
 
 const int _B=4e5;
 
-namespace A
-{
 //I don't know EOF
-	char Nc()
-	{
-		static char buf[_B],*p=buf,*q=buf;
-		return ((p==q)&&(q=buf+fread(buf,1,_B,stdin))==(p=buf))?0:*(p++);
-	}
-
-	int Read()
-	{
-		char ch;
-		while(!isdigit(ch=Nc()));
-		int sum=ch-48;
-		while(isdigit(ch=Nc()))
-			sum=sum*10+ch-48;
-		return sum;
-	}
-
+char Nc()
+{
+	static char buf[_B],*p=buf,*q=buf;
+	return ((p==q)&&(q=buf+fread(buf,1,_B,stdin))==(p=buf))?0:*(p++);
 }
 
-namespace B
+int Read()
+{
+	char ch;
+	while(!isdigit(ch=Nc()));
+	int sum=ch-48;
+	while(isdigit(ch=Nc()))
+		sum=sum*10+ch-48;
+	return sum;
+}
+
+namespace WoW
 {
 //I know EOF
 	bool BADEND;
