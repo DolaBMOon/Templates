7dcb54f07e1e21c65c1c69e53f673fdc8a0bc213
DOlaBMOon <dolabmoon@gmail.com>
2018-09-06 10:53:56 +0800

    PBDS

---
+++ PBDS/HashTable.cpp
@@ -0,0 +1,12 @@
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/hash_policy.hpp>
+
+using namespace __gnu_pbds;
+
+cc_hash_table<int,bool> A;//拉链法
+gp_hash_table<int,bool> B;//探查法
+
+int main()
+{
+	return 0;
+}
