6d013f0fd98ac2b9f92f66d13e0f7b75c3a99b69
DOlaBMOon <dolabmoon@gmail.com>
2018-09-11 14:56:30 +0800

    SAM

--- SAM/SAM.cpp
+++ SAM/SAM.cpp
@@ -10,39 +10,39 @@ using namespace std;
 
 const int N=1000000+10;
 
-int nxt[N][26],fa[N],dep[N],last=1,sz=1;
+int ch[N][26],fa[N],len[N],last=1,sz=1;
 
 void Append(int c)
 {
 	//Generalized
-	//if((nw=nxt[p=last][c])&&len[p]+1==len[nw])
+	//if((nw=ch[p=last][c])&&len[p]+1==len[nw])
 	//{
 	//	last=cur;
 	//	return;
 	//}
 	int p=last,nw=last=++sz;
-	dep[nw]=dep[p]+1;
-	for(;p&&!nxt[p][c];p=fa[p])
-		nxt[p][c]=nw;
+	len[nw]=len[p]+1;
+	for(;p&&!ch[p][c];p=fa[p])
+		ch[p][c]=nw;
 	if(!p)
 	{
 		fa[nw]=1;
 		return;
 	}
 
-	int q=nxt[p][c];
-	if(dep[p]+1==dep[q])
+	int q=ch[p][c];
+	if(len[p]+1==len[q])
 	{
 		fa[nw]=q;
 		return;
 	}
 	int clone=++sz;
-	dep[clone]=dep[p]+1;
-	memcpy(nxt[clone],nxt[q],104);
+	len[clone]=len[p]+1;
+	memcpy(ch[clone],ch[q],104);
 	fa[clone]=fa[q];
 	fa[nw]=fa[q]=clone;
-	for(;p&&nxt[p][c]==q;p=fa[p])
-		nxt[p][c]=clone;
+	for(;p&&ch[p][c]==q;p=fa[p])
+		ch[p][c]=clone;
 }
 
 int main()
